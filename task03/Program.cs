c198a26429f3fbff16f89ed20c7bf4915192c0f4
Aleksandr <rakov.alieksandr@inbox.ru>
2023-02-25 15:37:01 +0300

    задачи семинара законченны

--- task03/Program.cs
+++ task03/Program.cs
@@ -3,9 +3,22 @@
 // 3 -> Среда
 // 5 -> Пятница
 // 20 мин
- int number;
- Console.WriteLine("Введите номер дня недели");
- number = int.Parse(Console.ReadLine());
+int number;
+Console.WriteLine("Введите номер дня недели");
+number = Convert.ToInt32(Console.ReadLine());
 
- if 
+if (number == 1) Console.WriteLine("понедельник");
+else if (number == 2) Console.WriteLine("вторник");
+else if (number == 3) Console.WriteLine("среда");
+else if(number == 4) Console.WriteLine("четверг");
+else if(number == 5) Console.WriteLine("пятницпа");
+else if(number == 6) Console.WriteLine("суббота");
+else if(number == 7) Console.WriteLine("воскресенье");
+ 
+ else Console.WriteLine("неверное число");
+
+
+
+
+
 

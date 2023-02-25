659c457a4f59fec1550ec73bab5fcf875f401024
Aleksandr Rakov <124735228+AleksandrRakov@users.noreply.github.com>
2023-02-25 15:41:30 +0300

    Update Program.cs

--- task03/Program.cs
+++ task03/Program.cs
@@ -4,8 +4,16 @@
 // 5 -> Пятница
 // 20 мин
  int number;
- Console.WriteLine("Введите номер дня недели");
- number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели");
+number = Convert.ToInt32(Console.ReadLine());
 
- if 
+if (number == 1) Console.WriteLine("понедельник");
+else if (number == 2) Console.WriteLine("вторник");
+else if (number == 3) Console.WriteLine("среда");
+else if(number == 4) Console.WriteLine("четверг");
+else if(number == 5) Console.WriteLine("пятницпа");
+else if(number == 6) Console.WriteLine("суббота");
+else if(number == 7) Console.WriteLine("воскресенье");
+ 
+ else Console.WriteLine("неверное число");
 

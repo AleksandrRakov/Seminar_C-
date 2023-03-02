c12ebf3d941445369223560895d4d0a084ef0706
Aleksandr <rakov.alieksandr@inbox.ru>
2023-03-02 23:43:46 +0300

    Домашнее задание

--- task19/Program.cs
+++ task19/Program.cs
@@ -14,7 +14,6 @@ if (number < 100000)
     {
         bool Number(int num)
         {
-
             int num1 = num % 10;
             int num2 = num / 10 % 10;
             int num3 = num / 100 % 10;

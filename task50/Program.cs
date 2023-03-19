1d3d33b5929aeaba8804f44451459bc99616100b
Aleksandr <rakov.alieksandr@inbox.ru>
2023-03-20 02:28:28 +0300

    домашнее задание

---
+++ task50/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+Console.WriteLine("Введите номер строки");
+int numberi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int numberj = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool CheckingItemQuantity(int[,] matrix, int i, int j)
+{
+    if (i > matrix.GetLength(0) || j > matrix.GetLength(1))
+    {
+        return true;
+    }
+    if (i < 0 || j < 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int SearchingValueElement(int[,] matrix, int i, int j)
+{
+    int element = matrix[i - 1, j - 1];
+    return element;
+}
+
+
+int[,] array2d = CreateMatrixRndInt(10, 10, -100, 100);
+string checkingItemQuantity = CheckingItemQuantity(array2d, numberi, numberj)
+? "Такого элемента в массиве нет" :
+ $"Значение этого элемента -> {SearchingValueElement(array2d, numberi, numberj)}";
+Console.WriteLine(checkingItemQuantity );
+Console.WriteLine();
+PrintMatrix(array2d);
+
---
+++ task52/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMeanEachColumn(int[,] matrix)
+{
+    int size = 0;
+    double[] array = new double[matrix.GetLength(1)];
+
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            sum += matrix[i, j];
+         }
+        double arithmeticMean = sum / matrix.GetLength(1);
+        arithmeticMean = Math.Round(arithmeticMean, 1);
+        array[size] = arithmeticMean; 
+        size++;
+    }
+    return array;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write($"  [ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}; ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write($" ]");
+}
+
+int[,] array2d = CreateMatrixRndInt(6, 6, 0, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+double[] arithmeticMeanEachColumn = ArithmeticMeanEachColumn(array2d);
+Console.Write($"Среднее арифметическое каждого столбца:");
+PrintArray(arithmeticMeanEachColumn);
+
+
